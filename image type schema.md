76ec177dbf7dbbb6c6e05015e9343314d0909dc7
Adam Yeats <say@helloadam.me>
2012-05-25 16:39:01 +0100

    Add templating
    It works!

--- image type schema.md
+++ image type schema.md
@@ -3,21 +3,23 @@
 ## Generic link
 
     {
-      type: 'generic'
-      title: 'jQuery docs'
-      description: 'jquery is cool or something'
+      type: 'generic',
+      title: 'jQuery docs',
+      description: 'jquery is cool or something',
       who: 'Aaron',
-      url: 'http://jquery.com'
+      url: 'http://jquery.com',
+      when: '2:05pm'
     }
 
 ## Code link
 
     {
-      type: 'code'
-      title: 'Funky function'
-      code: 'function(){\n  doIt();\n}'
+      type: 'code',
+      title: 'Funky function',
+      code: 'function(){\n  doIt();\n}',
       who: 'Adam',
-      url: 'http://gist.github.com/248r28'
+      url: 'http://gist.github.com/248r28',
+      when: '2:05pm'
     }
 
 ## Image link
@@ -25,5 +27,6 @@
     {
       type: 'image',
       who: 'Prem',
-      url: 'example.com/image.png'
+      url: 'example.com/image.png',
+      when: '2:05pm'
     }
--- public/js/script.js
+++ public/js/script.js
@@ -17,26 +17,33 @@ function generateView(message) {
 	
 	if (message.type == 'code') {
 	
-		timTemp = "<article><pre>{{ }}</pre></article>";
+		timTemp = '<article class="codebox"><h2 class="codetitle">{{title}}</h2><span class="whowhen">Posted by {{who}} at {{when}}.</span><a class="codelink" href="{{url}}">See the code online</a><pre class="code">{{code}}</pre></article>';
 	
 	}
 	
 	if (message.type == 'generic') {
 		
-		
+		timTemp = '<article class="generalbox"><h2 class="generaltitle">{{title}}</h2><a class="generallink" href="{{url}}">{{url}}</a><h3><span class="whowhen">Posted by {{who}} at {{when}}.</span><p class="description">{{description}}</p></article>';
 		
 	}
 	
 	if (message.type == 'image') {
 		
-		
+		timTemp = '<article class="imgbox"><figure><img src="{{url}}" alt="{{name}}\'s Shared Image" /><figcaption><a class="imglink" href="{{url}}">{{url}}</a></figcaption></figure><span class="whowhen">Posted by {{who}} at {{when}}.</span><p class="description">{{description}}</p></article>';
 		
 	}
 	
-	tim(timTemp, data);
+	return tim(timTemp, message);
 	
 }
 
+/*
+function pushNotification() {
+	
+	
+	
+}
+*/
 
 
 
