3d8d2f2347a8b73f9ef0c9a8fa9fd5c1c2eb1d66
Aaron Acerboni <a.acerboni@gmail.com>
2012-05-31 14:40:25 +0100

    Implement server/client sockets

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ var app      = require('http').createServer(handler),
     io       = require('socket.io').listen(app),
     fs       = require('fs'),
     irc      = require('irc'),
-    expander = require('./expander');
+    expander = require('./expander'),
+    client;
 
 
-// IRC Bot snooping
 
-ircClient = new irc.Client('irc.freenode.net', 'Snoopy', {
+// IRC Bot snooping and socket emit
+
+client = new irc.Client('irc.freenode.net', 'Snoopy', {
   channels: ['#totallyunique'],
 });
 
-ircClient.addListener('message', function (from, to, message) {
+client.addListener('message', function (from, to, message) {
   var link = extractLink(message);
-  if(link){
-    expander.expand(extractLink(message), from).then(function (data) {
-      console.log(data);
-    });
-  }
+  if (link)
+    expander.expand(link, from);
 });
+
+
+// Find a link function
 
 function extractLink(message){
   var url = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi
@@ -29,18 +31,28 @@ function extractLink(message){
   return false;
 }
 
-// HTTP and Sockets
+
+// Socket connection
+
+io.sockets.on('connection', function (socket) {
+  expander.listen('expansion', function (data) {
+    socket.emit('link', data);
+  });
+});
+
+
+// HTTP
 
 app.listen(8888);
 
 function handler (req, res) {
-  var file = req.url.split('/')[1];
+  var path = req.url.replace('public/' + '');
+  path = (path === '/') ? 'index.html' : path;
 
-  fs.readFile(__dirname + '/public/' + file,
-  function (err, data) {
+  fs.readFile(__dirname + '/public/' + path, function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading' + file);
+      return res.end('Error loading ' + path);
     }
     res.writeHead(200);
     res.end(data);
