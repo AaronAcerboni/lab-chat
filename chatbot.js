d136aad510658f6de9fd680e4bce5c0bcda1a753
Aaron Acerboni <a.acerboni@gmail.com>
2012-06-01 11:11:15 +0100

    Abstract out ircClient, connected clients with event pattern

---
+++ chatbot.js
@@ -0,0 +1,36 @@
+var events  = require('events'),
+    emitter = new events.EventEmitter(),
+    irc     = require('irc'),
+    clients = [],
+    ircClient;
+
+
+// Chat bot
+
+ircClient = new irc.Client('irc.freenode.net', 'Snoopy', function (){
+  channels: ['#totallyunique2']
+});
+
+irc.addListener('message', function (from, to, message) {
+  var link = extractLink(message);
+  if (link) emitter.emit('foundLink', link, from);
+});
+
+
+// Returns a found link as String or false
+
+function extractLink(message){
+  var url = /(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/gi
+  .exec(message);
+  if(url){
+    return url[0];
+  }
+  return false;
+}
+
+
+// Exported listener for 'foundLink' event
+
+exports.listen = function (eventName, callback) {
+  emitter.on(eventName, callback);
+};
